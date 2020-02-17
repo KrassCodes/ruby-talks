10e4e386d93c95fa82282e286c60eb81648e8d02
Krass101 <k.iankov@gmail.com>
2020-02-17 23:20:39 +0000

    add rpsec test

--- lib/video_talks.rb
+++ lib/video_talks.rb
@@ -5,7 +5,9 @@ class Scraper
     include Wombat::Crawler
 
     base_url 'https://rubykaigi.org'
+
     path "/2017/schedule/#sep18"
+    
     gems do
         list "css=.schedule-item__title", :list
     end
