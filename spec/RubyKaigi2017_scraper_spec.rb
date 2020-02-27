c1f6dff2c9525687b5cb92fb80902ce7dca2f051
Krass101 <k.iankov@gmail.com>
2020-02-27 00:57:49 +0000

    clean up

--- spec/RubyKaigi2017_scraper_spec.rb
+++ spec/RubyKaigi2017_scraper_spec.rb
@@ -5,14 +5,16 @@ require 'webmock/rspec'
 
 describe Scraper_RubyKaigi2017 do
 
-    it 'collects the RubyKaigi 2017 conference name', :vcr do       
-            expect(subject.scrape).to include("conference_name" => "RubyKaigi 2017")
-    end
-
-    it 'collects the Keynote talk title, speaker name and github name of speaker', :vcr do     
+    it 'collects the speaker name, bio, talk title,talk description, link to supporting docs', :vcr do       
         expected = {
-            "conference_name" => "RubyKaigi 2017",
-            "talks" => a_hash_including("github"=>"@n0kada", "speaker"=>"nobu", "title"=>"Keynote")
+            "talk" => a_hash_including(
+                "conference_name"=>"RubyKaigi 2017",
+                "speaker_bio"=>"The patch monster",
+                 "speaker_name"=>"nobu",
+                 "talk_description"=>"Nobu's keynote speech",
+                 "talk_support_docs"=>"Making Ruby? ゆるふわRuby生活",
+                 "talk_title"=>"Keynote",
+                 "talk_video"=>"")
         }
             expect(subject.scrape).to include(expected)
     end
