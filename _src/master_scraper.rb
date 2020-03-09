4d660953014d84e35fb16b9d2634d99bf184b6e3
Krass101 <k.iankov@gmail.com>
2020-03-09 21:16:53 +0000

    file structure change

--- _src/master_scraper.rb
+++ _src/master_scraper.rb
@@ -7,6 +7,6 @@ require 'yaml'
     result = scraper_RubyKaigi2017.scrape
 
     # Save the scraped results to a YAML file, which will later be used by Jekyll to render onto a static page
-    File.open(File.dirname(File.expand_path('..', __FILE__)) + "/ruby-talks/_data/talks.yml", "w") { |file| file.write(result.to_yaml) }
+    File.open(File.dirname(File.expand_path('..', __FILE__)) + "/_data/talks.yml", "w") { |file| file.write(result.to_yaml) }
 
 # More conferences to be added below
